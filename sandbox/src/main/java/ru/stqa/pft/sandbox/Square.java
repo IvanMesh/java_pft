c457f36c2deb12690876d58b6bd6ba65c1d62202
IvanMesh <imeshcheriyakov@gmail.com>
2017-07-07 17:30:11 +0300

    Завершение первой лекции.

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by i.mescheryakov on 07.07.2017.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l){
+    this.l = l;
+  }
+  public double area () {
+    return this.l * this.l;
+  }
+}
