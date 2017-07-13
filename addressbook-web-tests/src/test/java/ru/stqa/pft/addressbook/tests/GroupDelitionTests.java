8b66d479513f220bf3588cb156e4b0e3c551f482
IvanMesh <imeshcheriyakov@gmail.com>
2017-07-13 18:53:14 +0300

    Распилили всё по разным классам.

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDelitionTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+
+public class GroupDelitionTests extends TestBase{
+
+
+    @Test
+    public void testGroupDelition() {
+       app.gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().deleteSelectedGroups();
+        app.getGroupHelper().returnToGroupPage();
+    }
+
+
+}
