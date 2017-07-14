6eb95930fe981fae8409c37215f280a1a36f2ca3
IvanMesh <imeshcheriyakov@gmail.com>
2017-07-14 16:46:59 +0300

    Revert "Создан общий базовый класс для классов-помощников"
    This reverts commit 5d0b0c1f54b66457aa2b8efeb63e1c97c33a6004.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -7,36 +7,44 @@ import ru.stqa.pft.addressbook.model.GroupData;
 /**
  * Created by i.mescheryakov on 13.07.2017.
  */
-public class GroupHelper extends HelperBase {
+public class GroupHelper {
+
+  private FirefoxDriver wd;
 
   public GroupHelper(FirefoxDriver wd) {
-    super(wd);
+    this.wd = wd;
   }
 
   public void returnToGroupPage() {
-    click(By.linkText("group page"));
+    wd.findElement(By.linkText("group page")).click();
   }
 
   public void submitGroupCreation() {
-    click(By.name("submit"));
+    wd.findElement(By.name("submit")).click();
   }
 
   public void fillGroupForm(GroupData groupData) {
-    type(By.name("group_name"), groupData.getName());
-    type(By.name("group_header"), groupData.getHeader());
-    type(By.name("group_footer"), groupData.getFooter());
+    wd.findElement(By.name("group_name")).click();
+    wd.findElement(By.name("group_name")).clear();
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
+    wd.findElement(By.name("group_header")).click();
+    wd.findElement(By.name("group_header")).clear();
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+    wd.findElement(By.name("group_footer")).click();
+    wd.findElement(By.name("group_footer")).clear();
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
   }
 
   public void initGroupCreation() {
-    click(By.name("new"));
+    wd.findElement(By.name("new")).click();
   }
 
   public void deleteSelectedGroups() {
-    click(By.xpath("//div[@id='content']/form/input[5]"));
+      wd.findElement(By.xpath("//div[@id='content']/form/input[5]")).click();
   }
 
   public void selectGroup() {
-    click(By.name("selected[]"));
+      wd.findElement(By.name("selected[]")).click();
 
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -6,15 +6,20 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by i.mescheryakov on 13.07.2017.
  */
-public class SessionHelper extends HelperBase {
+public class SessionHelper {
+  private FirefoxDriver wd;
 
   public SessionHelper(FirefoxDriver wd) {
 
-    super(wd);
+    this.wd = wd;
   }
   public void login(String password, String username) {
-    type(By.name("pass"), username);
-    type(By.name("pass"), password);
-    click(By.xpath("//form[@id='LoginForm']/input[3]"));
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
   }
 }
