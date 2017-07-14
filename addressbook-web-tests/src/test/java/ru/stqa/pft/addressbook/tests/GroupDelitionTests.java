a7d0e3335602ee8c25e8f7822c833ecf16ed4783
IvanMesh <imeshcheriyakov@gmail.com>
2017-07-14 20:55:20 +0300

    Создан тест GroupModificationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDelitionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDelitionTests.java
@@ -8,9 +8,10 @@ public class GroupDelitionTests extends TestBase {
     @Test
     public void testGroupDelition() {
        app.getNavigationHelper().gotoGroupPage();
-        app.getGroupHelper().selectGroup();
-        app.getGroupHelper().deleteSelectedGroups();
-        app.getGroupHelper().returnToGroupPage();
+       app.getGroupHelper().selectGroup();
+       app.getGroupHelper().deleteSelectedGroups();
+       app.getGroupHelper().returnToGroupPage();
+
     }
 
 
