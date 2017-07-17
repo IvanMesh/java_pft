7ef1dae7de21cfd7d11883bd7fce3104769d895f
IvanMesh <imeshcheriyakov@gmail.com>
2017-07-17 13:28:26 +0300

    Создан класс ContactModificationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -39,4 +39,12 @@ public class ContactHelper extends HelperBase{
   public void selectContact() {
     click(By.name("selected[]"));
   }
+
+  public void initContactModification() {
+    click(By.xpath("//div/div[4]/form[2]/table/tbody/tr[2]/td[8]/a/img"));
+  }
+
+  public void submitContactModification() {
+    click(By.name("update"));
+  }
 }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+/**
+ * Created by i.mescheryakov on 17.07.2017.
+ */
+public class ContactModificationTests extends TestBase {
+
+  @Test
+  public void testContactModification() {
+
+    app.getNavigationHelper().gotoHomePage();
+    app.getContactHelper().selectContact();
+    app.getContactHelper().initContactModification();
+    app.getContactHelper().fillNewContactForm(new ContactData("Иван", "Юрьевич", "Мещеряков", "Slickan", "МО, Ленинский р-н, пос. Развилка", "+7(999)813-80-83", "imeshcheriyakov@gmail.com"));
+    app.getContactHelper().submitContactModification();
+  }
+}
