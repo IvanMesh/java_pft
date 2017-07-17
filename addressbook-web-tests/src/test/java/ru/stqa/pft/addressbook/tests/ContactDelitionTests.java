ee1268e81b25d7d38f8eba22e14264d1c07eead5
IvanMesh <imeshcheriyakov@gmail.com>
2017-07-17 13:21:22 +0300

    Создан класс  ContactDeletionTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDelitionTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook.tests;
+
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by i.mescheryakov on 17.07.2017.
+ */
+public class ContactDelitionTests extends TestBase {
+
+  @Test
+  public void testContactDelition() {
+
+    app.getNavigationHelper().gotoHomePage();
+    app.getContactHelper().selectContact();
+    app.getContactHelper().initContactDeletion();
+  }
+
+}
