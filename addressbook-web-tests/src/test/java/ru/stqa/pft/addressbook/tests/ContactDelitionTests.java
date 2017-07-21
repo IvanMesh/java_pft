3ef1620f079bcce398aceb3e96ad10a2d95eb106
IvanMesh <imeshcheriyakov@gmail.com>
2017-07-21 13:16:06 +0300

    Тест удаления контакта теперь может создавать контакт, если его не обнаружит

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDelitionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDelitionTests.java
@@ -3,6 +3,7 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
 
 /**
  * Created by i.mescheryakov on 17.07.2017.
@@ -14,7 +15,7 @@ public class ContactDelitionTests extends TestBase {
 
     app.getNavigationHelper().gotoHomePage();
     if (! app.getContactHelper().isThereAContact()) {
-
+      app.getContactHelper().createContact(new ContactData("Иван", "Юрьевич", "Мещеряков", "Slickan", "МО, Ленинский р-н, пос. Развилка", "+7(999)813-80-83", "imeshcheriyakov@gmail.com", null), false);
       }
     app.getContactHelper().selectContact();
     app.getContactHelper().initContactDeletion();
