1de940b70164da237ae18b805887e2d8a3e0a5b0
IvanMesh <imeshcheriyakov@gmail.com>
2017-08-01 13:30:52 +0300

    Revert "Я устал, я хочу выходных"
    This reverts commit 0b44d920a3b96fc3f3f23c218316a52493278340.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -5,7 +5,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -22,9 +21,6 @@ public class GroupCreationTests extends TestBase {
 
     group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
-    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
-    before.sort(byId);
-    after.sort(byId);
-    Assert.assertEquals(after, before);
+    Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
   }
 }
