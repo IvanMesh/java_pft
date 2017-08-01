1638c4467c76340f6ab7156181f96141751607c2
IvanMesh <imeshcheriyakov@gmail.com>
2017-08-01 22:41:09 +0300

    Сортировка списка в создании и модификации контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -9,12 +9,14 @@ public class ContactData {
   private final String почта;
   private String group;
 
+
   public void setId(int id) {
+
     this.id = id;
   }
 
   public ContactData(String Имя, String Фамилия, String Адрес, String Телефон, String Почта, String group) {
-    this.id = 0;
+    this.id = Integer.MAX_VALUE;
     this.имя = Имя;
     this.фамилия = Фамилия;
     this.адрес = Адрес;
@@ -61,32 +63,6 @@ public class ContactData {
   public String getGroup() { return group; }
 
   @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-
-    ContactData that = (ContactData) o;
-
-    if (id != that.id) return false;
-    if (имя != null ? !имя.equals(that.имя) : that.имя != null) return false;
-    if (фамилия != null ? !фамилия.equals(that.фамилия) : that.фамилия != null) return false;
-    if (адрес != null ? !адрес.equals(that.адрес) : that.адрес != null) return false;
-    if (телефон != null ? !телефон.equals(that.телефон) : that.телефон != null) return false;
-    return почта != null ? почта.equals(that.почта) : that.почта == null;
-  }
-
-  @Override
-  public int hashCode() {
-    int result = id;
-    result = 31 * result + (имя != null ? имя.hashCode() : 0);
-    result = 31 * result + (фамилия != null ? фамилия.hashCode() : 0);
-    result = 31 * result + (адрес != null ? адрес.hashCode() : 0);
-    result = 31 * result + (телефон != null ? телефон.hashCode() : 0);
-    result = 31 * result + (почта != null ? почта.hashCode() : 0);
-    return result;
-  }
-
-  @Override
   public String toString() {
     return "ContactData{" +
             "id='" + id + '\'' +
@@ -99,4 +75,29 @@ public class ContactData {
             '}';
 
   }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    ContactData that = (ContactData) o;
+
+    if (имя != null ? !имя.equals(that.имя) : that.имя != null) return false;
+    if (фамилия != null ? !фамилия.equals(that.фамилия) : that.фамилия != null) return false;
+    if (адрес != null ? !адрес.equals(that.адрес) : that.адрес != null) return false;
+    if (телефон != null ? !телефон.equals(that.телефон) : that.телефон != null) return false;
+    return почта != null ? почта.equals(that.почта) : that.почта == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = имя != null ? имя.hashCode() : 0;
+    result = 31 * result + (фамилия != null ? фамилия.hashCode() : 0);
+    result = 31 * result + (адрес != null ? адрес.hashCode() : 0);
+    result = 31 * result + (телефон != null ? телефон.hashCode() : 0);
+    result = 31 * result + (почта != null ? почта.hashCode() : 0);
+    return result;
+  }
+
 }
