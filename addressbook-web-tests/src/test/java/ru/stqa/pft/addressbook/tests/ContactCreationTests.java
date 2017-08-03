f9c7c12af8bc7ac58b72b5df58bd8ad51ca591b2
IvanMesh <imeshcheriyakov@gmail.com>
2017-08-03 18:10:15 +0300

    Преобразование списков в множества в тестах для групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -13,11 +13,7 @@ public class ContactCreationTests extends TestBase {
     @Test (enabled = false)
     public void testContactCreation() {
         List<ContactData> before = app.getContactHelper().getContactList();
-        app.goTo().gotoAddNewPage();
-        ContactData contact = new ContactData("Иван",  "Мещеря1ков",  "МО, Ленинский р-н, пос. Развилка", "79998138083", "imeshcheriyakov@gmail.com", "1");
-        app.getContactHelper().fillNewContactForm(contact, true);
-        app.getContactHelper().submitNewContactCreation();
-        app.goTo().gotoHomePage();
+        ContactData contact = create();
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
@@ -26,5 +22,14 @@ public class ContactCreationTests extends TestBase {
         before.sort(byId);
         after.sort(byId);
         Assert.assertEquals(before, after);
+    }
+
+    private ContactData create() {
+        app.goTo().gotoAddNewPage();
+        ContactData contact = new ContactData("Иван",  "Мещеря1ков",  "МО, Ленинский р-н, пос. Развилка", "79998138083", "imeshcheriyakov@gmail.com", "1");
+        app.getContactHelper().fillNewContactForm(contact, true);
+        app.getContactHelper().submitNewContactCreation();
+        app.goTo().gotoHomePage();
+        return contact;
     }
 }
